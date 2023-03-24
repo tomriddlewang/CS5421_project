f3016e1c7902084c9cc77cb01a4ce2098261af23
Wang Haotian <tomriddlewang@gmail.com>
2023-03-24 16:34:50 +0800

    first commit

---
+++ angularapp/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public forecasts?: WeatherForecast[];
+
+  isDataReady: boolean = false;
+  testString?: string[];
+
+  constructor(http: HttpClient) {
+    //http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
+    //  this.forecasts = result;
+    //}, error => console.error(error));
+  }
+
+  title = 'angularapp';
+
+  ngOnInit(){
+    console.log("AHHHHHHHHA");
+  }
+
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+interface ISchema {
+
+}
